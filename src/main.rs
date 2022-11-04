753c899c422a1fd8d523f9968de0b5dbbb5314f5
Breno <breno.cn@hotmail.com>
2022-11-03 22:11:50 -0300

    escrita inicial da fita

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,20 @@ fn main() {
 
     turing_machine.load_yaml_file(String::from("machine.yaml"));
     println!("{:?}", turing_machine);
+
+    let stdin = std::io::stdin();
+    let mut buffer = String::new();
+    stdin.read_line(&mut buffer).unwrap();
+
+    let words = buffer.clone().to_string();
+    let input = words
+        .trim()
+        .split(" ")
+        .map(|word| { word.to_string() })
+        .collect::<Vec<String>>();
+
+    println!("{:?}", input);
+
+    turing_machine.write_tape(input);
+    println!("{:?}", turing_machine);
 }
--- src/tm/tape.rs
+++ src/tm/tape.rs
@@ -4,7 +4,8 @@ pub enum TapeMovement { L, R }
 #[derive(Debug)]
 pub struct Tape {
     data: Vec<String>,
-    position: usize
+    position: usize,
+    tape_size: usize
 }
 
 impl TapeMovement {
@@ -27,8 +28,17 @@ impl Tape {
 
         Tape {
             data: vec![String::from(""); tape_size],
-            position: initial_tape_pos
+            position: initial_tape_pos,
+            tape_size
         }
+    }
+
+    pub fn reset_tape(&mut self) {
+        self.position = self.tape_size / 2;
+    }
+
+    pub fn write_tape(&mut self, input: Vec<String>) {
+        self.data = input;
     }
 
 }
